53e182baae3d1100e2ec46c9b922c607561465ea
Khuzaima12 Asif <50294889-khuzaima12@users.noreply.replit.com>
2026-07-24 17:14:37 +0000

    border updated

--- artifacts/menu/src/components/avatar/IcBdr.tsx
+++ artifacts/menu/src/components/avatar/IcBdr.tsx
@@ -1,13 +1,11 @@
 import "./shape/ic.css";
-import "./avt-bg.css";
-import "./Avt.css";
 
 interface IcBdrPr { children: React.ReactNode; }
 
 export function IcBdr({ children }: IcBdrPr) {
   return (
     <div className="ic-bdr">
-      <div className="avt-shp avt-bg">{children}</div>
+      <div className="ic-inn">{children}</div>
       <div className="ic-bar" aria-hidden />
     </div>
   );
